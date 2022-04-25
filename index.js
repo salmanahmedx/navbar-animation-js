f4c712fd87f58e90a57ece6847cac0c01e91f9c9
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-26 00:20:12 +0600

    navbar animation

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const ul = document.querySelector(".ul");
 const nav = document.querySelector("nav");
 const black = document.querySelector(".black")
 const grey = document.querySelector(".grey")
+const darkGrey = document.querySelector(".dark-grey")
 
 menu.addEventListener('click', e => {
     ul.classList.toggle("visible")
     black.classList.toggle("visible")
     grey.classList.toggle("visible")
+    darkGrey.classList.toggle("visible")
 })
