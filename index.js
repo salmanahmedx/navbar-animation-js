25b5baea7cd4c6f768ff7f49fc5871cb5b767337
Salman Ahmed <salmanahmedx@hotmail.com>
2022-05-05 13:54:38 +0600

    font awesome issue solved

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const nav = document.querySelector("nav");
 const black = document.querySelector(".black")
 const grey = document.querySelector(".grey")
 const darkGrey = document.querySelector(".dark-grey")
+const cross = document.querySelector(".cross")
 
 menu.addEventListener('click', e => {
     ul.classList.toggle("visible")
     black.classList.toggle("visible")
     grey.classList.toggle("visible")
     darkGrey.classList.toggle("visible")
+    menu.classList.add('hide')
+    cross.classList.remove("hide")
+})
+
+cross.addEventListener("click", e => {
+    ul.classList.toggle("visible")
+    black.classList.toggle("visible")
+    grey.classList.toggle("visible")
+    darkGrey.classList.toggle("visible")
+    cross.classList.add("hide")
+    menu.classList.remove("hide")
 })
